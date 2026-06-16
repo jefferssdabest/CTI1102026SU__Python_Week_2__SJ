e635e88e881add9886ef5da2b534579c0a41c905
jefferssdabest <jefferss9070@student.faytechcc.edu>
2026-06-15 23:05:25 -0400

    Create P1LAB1_SamariJeffers.py

---
+++ P1LAB1_SamariJeffers.py
@@ -0,0 +1,17 @@
+# Samari Jeffers
+# 6/11/2026
+# P1LAB1
+# 
+#
+# This program takes user input and prints a welome message
+
+def main():
+   first_name = input("Enter your first name: ")
+   last_name = input("Enter your last name: ")
+
+   print(f"Hello, {first_name} {last_name}! Welcome to CTI-110!")
+   return
+
+main()
+
+#input("Press Enter to continue...")
